e68a6bd066f7a1ebd14ee10a94b0fa41513e7c5e
valentinavalenzi <92260481+valentinavalenzi@users.noreply.github.com>
2023-11-18 15:55:28 -0300

    latest transactions service

---
+++ src/service/apis.js
@@ -0,0 +1,12 @@
+const fetchLatestTransactions = async () => {
+    try {
+        const response = await fetch('http://localhost:8080/api/latest-transactions');
+        const data = await response.json();
+        console.log('Latest Transactions:', data);
+        // Process the transactions in your frontend UI
+    } catch (error) {
+        console.error('Error fetching latest transactions:', error);
+        // Handle error in fetching data from the backend
+    }
+};
+
